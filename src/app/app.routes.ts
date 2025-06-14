1c88209d23c5998bd1bcaf158a1a617e209bd5ec
Selvin Medina <selvinmedina0016@gmail.com>
2025-06-13 22:46:54 -0600

    setup initial app structure with tabs, routes and NgRx base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'feed',
+    loadComponent: () => import('./pages/feed/feed.page').then( m => m.FeedPage)
+  },
+  {
+    path: 'user-search',
+    loadComponent: () => import('./pages/user-search/user-search.page').then( m => m.UserSearchPage)
+  },
 ];
